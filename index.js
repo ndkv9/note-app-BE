5b742099bd000ec1ef3cbd37e917a19842f3f86d
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-04 13:13:48 +0200

    initial state

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+const notes = [
+	{
+		id: 1,
+		content: 'HTML is easy',
+		date: '2019-05-30T17:30:31.098Z',
+		important: true,
+	},
+	{
+		id: 2,
+		content: 'Browser can execute only Javascript',
+		date: '2019-05-30T18:39:34.091Z',
+		important: false,
+	},
+	{
+		id: 3,
+		content: 'GET and POST are the most important methods of HTTP protocol',
+		date: '2019-05-30T19:20:14.298Z',
+		important: true,
+	},
+]
+
+app.get('/', (req, res) => {
+	res.send('<h3>hello darkness</h3>')
+})
+
+app.get('/api/notes', (req, res) => {
+	res.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
