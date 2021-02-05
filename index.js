850c7a29de034c7dbdae9a6e2488062677638791
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-05 15:32:31 +0200

    modify PORT value to environment variables

--- index.js
+++ index.js
@@ -85,5 +85,5 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
