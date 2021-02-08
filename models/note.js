9739e2403bdd2dac6288d7932a3b7c1b779deae8
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-08 15:52:08 +0200

    add models module

---
+++ models/note.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose
+	.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(result => {
+		console.log('connected to MongoDB')
+	})
+	.catch(err => console.log('error connecting to MongoDB', err.message))
+
+const noteSchema = new mongoose.Schema({
+	content: String,
+	date: Date,
+	important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+	transform: (doc, returnedObj) => {
+		returnedObj.id = returnedObj._id.toString()
+		delete returnedObj._id
+		delete returnedObj.__v
+	},
+})
+
+module.exports = mongoose.model('Note', noteSchema)
