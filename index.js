138eca0f0a313f950d3e7534b06263865826cdbd
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-09 12:08:20 +0200

    add toggle important functionality to DB

--- index.js
+++ index.js
@@ -30,8 +30,10 @@ app.post('/api/notes', (req, res) => {
 })
 
 // retrieve all notes
-app.get('/api/notes', (req, res) => {
-	Note.find({}).then(notes => res.json(notes))
+app.get('/api/notes', (req, res, next) => {
+	Note.find({})
+		.then(notes => res.json(notes))
+		.catch(err => next(err))
 })
 
 // retrieve a single note
@@ -48,13 +50,28 @@ app.get('/api/notes/:id', (req, res, next) => {
 		.catch(err => next(err))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-	const id = Number(req.params.id)
-	notes = notes.filter(note => note.id !== id)
-
-	res.status(204).end()
+// delete a single note by id
+app.delete('/api/notes/:id', (req, res, next) => {
+	const id = req.params.id
+	Note.findByIdAndRemove(id)
+		.then(result => res.status(204).end())
+		.catch(err => next(err))
 })
 
+// toggle the important of a note
+app.put('/api/notes/:id', (req, res, next) => {
+	const body = req.body
+	const note = {
+		content: body.content,
+		important: body.important,
+	}
+
+	Note.findByIdAndUpdate(req.params.id, note, { new: true })
+		.then(updatedNote => {
+			res.json(updatedNote)
+		})
+		.catch(err => next(err))
+})
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'unknown endpoint' })
 }
