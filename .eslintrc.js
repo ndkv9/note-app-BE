fa404d4ae8a36aafa9c3690ab203c6ba3b50a0b2
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-10 14:54:45 +0200

    modify eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,11 @@ module.exports = {
 		'linebreak-style': ['error', 'unix'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'never'],
+		eqeqeq: 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': ['error', 'always'],
+		'arrow-spacing': ['error', { before: true, after: true }],
+		'no-console': 0,
+		'linebreak-style': ['error', { before: true, after: true }],
 	},
 }
