d651eb1988f012dc4b96f76288f2ea1a39f4ffa6
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-10 14:41:49 +0200

    add .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-};
+	env: {
+		commonjs: true,
+		es6: true,
+		node: true,
+	},
+	extends: 'eslint:recommended',
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	rules: {
+		indent: ['error', 2],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+	},
+}
