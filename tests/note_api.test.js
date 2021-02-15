2962e2d65037263e810569dc4b3c13f6ebcd07ef
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-15 19:30:39 +0200

    add test cases for adding new notes

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -45,6 +45,37 @@ test('a specific note is within the returned notes', async () => {
 	const contents = response.body.map(r => r.content)
 	expect(contents).toContain('Browser can execute only Javascript')
 })
+
+test('a valid note can be added', async () => {
+	const newNote = {
+		content: 'async/await simplifies making async calls',
+		important: true,
+	}
+
+	await api
+		.post('/api/notes', newNote)
+		.send(newNote)
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
+
+	const response = await api.get('/api/notes')
+	const contents = response.body.map(r => r.content)
+
+	expect(response.body).toHaveLength(initialNotes.length + 1)
+	expect(contents).toContain(newNote.content)
+})
+
+test('a note without content will not be saved', async () => {
+	const newNote = {
+		important: true,
+	}
+
+	await api.post('/api/notes', newNote).send(newNote).expect(400)
+
+	const response = await api.get('/api/notes')
+	expect(response.body).toHaveLength(initialNotes.length)
+})
+
 afterAll(() => {
 	mongoose.connection.close()
 })
