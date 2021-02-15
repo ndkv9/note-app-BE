c66f44dc5a28412705e4b6a53cd9f4272a1bb4f9
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-15 17:36:12 +0200

    add test cases to note api test

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -4,6 +4,28 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Note = require('../models/note')
+const initialNotes = [
+	{
+		content: 'HTML is easy',
+		date: new Date(),
+		important: false,
+	},
+	{
+		content: 'Browser can execute only Javascript',
+		date: new Date(),
+		important: true,
+	},
+]
+
+beforeEach(async () => {
+	await Note.deleteMany({})
+	let oneObj = new Note(initialNotes[0])
+	await oneObj.save()
+	oneObj = new Note(initialNotes[1])
+	await oneObj.save()
+})
+
 test('notes are returned as json', async () => {
 	await api
 		.get('/api/notes')
@@ -11,6 +33,18 @@ test('notes are returned as json', async () => {
 		.expect('Content-Type', /application\/json/)
 })
 
+test('all notes are returned', async () => {
+	const response = await api.get('/api/notes')
+
+	expect(response.body).toHaveLength(initialNotes.length)
+})
+
+test('a specific note is within the returned notes', async () => {
+	const response = await api.get('/api/notes')
+
+	const contents = response.body.map(r => r.content)
+	expect(contents).toContain('Browser can execute only Javascript')
+})
 afterAll(() => {
 	mongoose.connection.close()
 })
