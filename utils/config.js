baf5c3c88202091113b2ed7116ab7b786a65478d
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-26 16:31:39 +0200

    modify testing route

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
